381fc405dd6c47be54bf694511c3efeb0ef500ee
kai <supon3060@gmail.com>
2020-10-19 21:08:06 +0800

    Solve leap

---
+++ julia/leap/leap.jl
@@ -0,0 +1,11 @@
+"""
+    is_leap_year(year)
+
+Return `true` if `year` is a leap year in the gregorian calendar.
+
+"""
+function is_leap_year(year)
+    return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) 
+end
+
+is_leap_year(2000)
